b9c132c31848fe93758c6b243c3c2e8b616129cc
Samuel Ndukwe <39251068+ndukwesamuel@users.noreply.github.com>
2025-12-04 11:15:49 +0100

    fox

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1";
-// const apiUrl = "http://localhost:8080/api/v1";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
